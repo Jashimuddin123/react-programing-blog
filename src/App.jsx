5c1019a35a8460d2d42aea4698756e338f9a4163
jashimuddin123 <adnanmahmud3394@gmail.com>
2024-01-31 00:25:53 +0600

    aded blogs components

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-red-500 text-2xl text-center p-4'>React Programing Blog Site    </h1>
+      <Header></Header>
+      <Blogs></Blogs>
+
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-semibold ">Lets Start Coding</h1>
+        </div>
+    );
+};
+
+export default Header;
